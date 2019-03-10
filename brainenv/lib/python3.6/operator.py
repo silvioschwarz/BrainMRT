a9ecbda69f67829ab89940d22c93c0652e6d7006
Silvio Schwarz <vio.schwarz89@gmail.com>
2019-03-10 05:19:34 +0100

    set up dash app

---
+++ brainenv/lib/python3.6/operator.py
@@ -0,0 +1,1 @@
+/home/silvio/anaconda3/lib/python3.6/operator.py
--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #! /bin/sh
 
-#conda activate py3
-conda install ipykernel            # install Python kernel (ipykernel) into the conda environment
-python -m ipykernel install        # install python kernel into nteract's available kernel list
-conda install -c conda-forge scikit-image
-
 conda install python=3 plotly scikit-learn numpy scipy pydicom vtk scikit-image pillow gdcm pytest 
 conda install python=3 tornado==4.5.3
 conda install python=3 pydicom --channel conda-forge 
@@ -13,3 +8,8 @@ conda install python=3 pydicom --channel conda-forge
 sudo apt-get install python-skimage
 sudo apt-get install build-essential cython
 
+
+#conda activate py3
+conda install ipykernel            # install Python kernel (ipykernel) into the conda environment
+python -m ipykernel install        # install python kernel into nteract's available kernel list
+conda install -c conda-forge scikit-image
